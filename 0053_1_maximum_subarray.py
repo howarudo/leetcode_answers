2da3d7c04ada7ad33fc16d3400ae22eaf3afb146
Howard Tangkulung <howardtangkulung@gmail.com>
2023-09-13 00:13:21 +0900

    0053_1 maximum subarray

---
+++ 0053_1_maximum_subarray.py
@@ -0,0 +1,18 @@
+# Problem link: https://leetcode.com/problems/maximum-subarray
+# Date: 2023/09/13
+# Big-O: O(n)
+# Overview:
+#   - Traverses the whole array to find the max sum. Restarts summation when it becomes negative
+#   - Divide and conquer is better
+# <============ SOLUTION ============>
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        curr_sum = 0
+        max_sum = nums[0]
+        for i in range(len(nums)):
+            curr_sum += nums[i]
+            max_sum = max(max_sum, curr_sum)
+            if curr_sum < 0:
+                curr_sum = 0
+        return max_sum
