d2a3bb1ae9bfe4777a13a77f5d25cf8368394c7f
Howard Tangkulung <howardtangkulung@gmail.com>
2023-09-13 14:40:41 +0900

    0054 completed

---
+++ 0054_spiral_matrix.py
@@ -0,0 +1,35 @@
+# Problem link: https://leetcode.com/problems/spiral-matrix/
+# Date: 2023/09/13
+# Big-O: O(mn) (m and n are the length and width of matrix)
+# Overview:
+#   - Have four boundaries, top, bottom , left and right
+#   - Traversing the whole matrix in 4 directions -> using 4 for loops, bounded by boundaries
+#   - Double check condition after first 2 loops in case l = r + 1
+# <============ SOLUTION ============>
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        l, r = 0, len(matrix[0])
+        top, bottom = 0, len(matrix)
+        res = []
+        while l < r and top < bottom:
+            for i in range(l, r):
+                res.append(matrix[top][i])
+            top += 1
+
+            for i in range(top, bottom):
+                res.append(matrix[i][r - 1])
+            r -= 1
+
+            if not (l < r and top < bottom):
+                break
+
+            for i in range(r - 1, l - 1, -1):
+                res.append(matrix[bottom - 1][i])
+            bottom -= 1
+
+            for i in range(bottom -1, top - 1, -1):
+                res.append(matrix[i][l])
+            l += 1
+
+        return res
