44a75e97bc50e9de3253259c44d7b07185ae35be
Howard Tangkulung <howardtangkulung@gmail.com>
2023-09-14 21:07:10 +0900

    55 jump game

---
+++ 55_jump_game.py
@@ -0,0 +1,14 @@
+# Problem link: https://leetcode.com/problems/jump-game/
+# Date: 2023/09/14
+# Big-O: O(n)
+# Overview:
+#   - Greed from behind, recursively change the "goal" of jumps
+# <============ SOLUTION ============>
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        goal = len(nums) - 1
+        for i in range(len(nums) - 1, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i
+        return goal == 0
